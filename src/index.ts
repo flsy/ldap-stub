68e930a99853122bbbc5f494dab73aea07b31df6
Honza Smid <jansmidhonza@gmail.com>
2020-07-15 15:23:46 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const openLdapServer = () => 'ho';
+export const openLdapClient = () => 'ho';
+
+export const activeDirectoryServer = () => 'ho';
+export const activeDirectoryClient = () => 'ho';
